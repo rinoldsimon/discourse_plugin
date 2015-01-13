96d8de9116f12eb3c2ca7497c5bc86d6e7d34754
rinoldsimon <crisrinold@gmail.com>
2015-01-13 20:28:48 +0530

    try1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
   def index
-  	@client = DiscourseApi::Client.new("http://try.discourse.org")
-    @client.api_key = ""
-    @client.api_username = ""
   end
 end
