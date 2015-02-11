0b816b98282dd27b52a7b6e9713f64a549dba3c5
rinoldsimon <crisrinold@gmail.com>
2015-02-11 22:39:05 +0530

    screen scraping

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
   	require 'rubygems'
     require 'nokogiri'
     require 'open-uri'
+
+    url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=batman&Find.x=0&Find.y=0&Find=Find"
+    @doc = Nokogiri::HTML(open(url))
+
   end
 end
