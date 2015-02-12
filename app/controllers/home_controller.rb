d102cdc31f5d735b0c693043ac0800591c19398a
rinoldsimon <crisrinold@gmail.com>
2015-02-12 19:19:41 +0530

    search for nodes by css and xpath

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,7 @@ class HomeController < ApplicationController
     url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=batman&Find.x=0&Find.y=0&Find=Find"
     @doc = Nokogiri::HTML(open(url))
 
+    # Fetch and parse HTML document
+    @doc1 = Nokogiri::HTML(open('http://www.nokogiri.org/tutorials/installing_nokogiri.html'))
   end
 end
