1a727b10aa5cb35156a6af43cde02105add4d326
rinoldsimon <crisrinold@gmail.com>
2015-02-14 08:43:07 +0530

    top post from discourse modified

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,9 @@ class HomeController < ApplicationController
     require 'nokogiri'
     require 'open-uri'
 
-    url = "http://www.walmart.com/search/search-ng.do?search_constraint=0&ic=48_0&search_query=batman&Find.x=0&Find.y=0&Find=Find"
-    @doc = Nokogiri::HTML(open(url))
-
     # Fetch and parse HTML document
-    @doc1 = Nokogiri::HTML(open('http://www.nokogiri.org/tutorials/installing_nokogiri.html'))
-
-    @doc2 = Nokogiri::HTML(open('http://try.discourse.org/top'))
+    url = "http://try.discourse.org/top"
+    @dparse = Nokogiri::HTML(open(url))
+    @tpost = @dparse.xpath('//a')
   end
 end
